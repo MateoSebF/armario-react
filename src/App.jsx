33a9efe0891b9869dee29dcbc2a1b9f0e594bead
Juanes <juan.ortiz-p@mail.escuelaing.edu.co>
2024-04-21 18:05:47 -0500

    Profile Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Importa Routes en lugar de Switch
-import HomePage from './pages/HomePage';
-import ProfilePage from './pages/ProfilePage';
-import './styles/App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Wardrobe from './components/pages/Wardrobe';
+import Calendar from './components/pages/Calendar';
+import Community from './components/pages/Community';
+import Profile from './components/pages/Profile';
 
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Routes> 
-          <Route path="/" element={<HomePage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+  return ( 
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home></Home>}/>
+        <Route path='/Wardrobe' element={<Wardrobe></Wardrobe>}/>
+        <Route path='/Calendar' element={<Calendar></Calendar>}/>
+        <Route path='/Community' element={<Community></Community>}/>
+        <Route path='/Profile' element={<Profile></Profile>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/components/pages/Calendar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../secondary/NavBar';
+
+const Calendar = () => {
+    return (
+        <div className='col-12'>
+            <NavBar/>
+        </div>
+    );
+}
+
+export default Calendar;
