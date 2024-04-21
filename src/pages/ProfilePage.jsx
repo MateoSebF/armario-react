2cddd9ba0f54a0cc7d3b332df5c264920b306d49
Juanes <juan.ortiz-p@mail.escuelaing.edu.co>
2024-04-21 14:38:00 -0500

    Profile Basic Page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import HeaderComponent from '../components/HeaderComponent';
+import ProfileCard from '../components/ProfileCard';
+import profileImage from '../profile.jpeg'; 
 
 const ProfilePage = () => {
+  const profileData = {
+    name: 'Murcia',
+    username: 'nosejsjs',
+    numItems: 20,
+    numOutfits: 10,
+    profileImage: profileImage, 
+  };
+
   return (
     <div>
       <HeaderComponent />
+      <div className="profile-container">
+        <ProfileCard profileData={profileData} />
+      </div>
     </div>
   );
 };
