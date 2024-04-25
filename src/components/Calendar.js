043fb7356cdab29ea71723ebd5707108e3295705
RichiPichi <ricardovillamizar66@gmail.com>
2024-04-24 21:32:09 -0500

    1.0.0 Community

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import NavBar from './secondary/NavBar';
 
+
 const Calendar = () => {
     return (
         <div className='col-12'>
             <NavBar/>
+            <div className='row'>
+                <div className='col-4'>
+                    {/* Contenido para el lado izquierdo (30%) */}
+                </div>
+                <div className='col-8'>
+
+                </div>
+            </div>
+            <div className='row'>
+                <div className='col-12'>
+                    {/* Espacio inferior */}
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/components/secondary/LogoCommunity.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Carousel from './CarouselI';
+import Button from 'react-bootstrap/Button';
+import './styles/Outfit.css';
+
+const Outfit = () => {
+    const heart = './images/heart.png';
+}
+
+export default Outfit;
