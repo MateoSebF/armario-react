a2a29b5139b7fd734a8b40273bbbf7f995158f54
Naetffy <mateosebastianforerofuentes@gmail.com>
2024-05-02 00:36:21 -0500

    4.0.0

--- src/components/secondary/CarouselI.js
+++ src/components/secondary/CarouselI.js
@@ -2,17 +2,18 @@ import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import './styles/Carousel.css';
 import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
-const CarouselI = ({ images }) => {
+const CarouselI = ({ clothes }) => {
     return (
         <Carousel
             prevIcon={<FaLongArrowAltLeft id='left-arrow'/>}
             nextIcon={<FaLongArrowAltRight id='right-arrow'/>}
             interval={null}
-            indicators={false}
-        >
-            {images.map((image, index) => (
+            indicators={false}>
+            {clothes.map((clothes, index) => (
                 <Carousel.Item key={index}>
-                    <img src={image} alt="" className="custom-carousel-image" />
+                    {clothes.image && (
+                        <img src={`data:image/jpeg;base64,${clothes.image}`} alt="" className="custom-carousel-image" />
+                    )}
                 </Carousel.Item>
             ))}
         </Carousel>
