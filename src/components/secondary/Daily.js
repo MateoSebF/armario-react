fea64bf44f7ace5c2a0be6bb5a03b665c87c2313
juaneortiz1 <97971732+juaneortiz1@users.noreply.github.com>
2024-05-08 12:37:34 -0500

    Update Daily.js

--- src/components/secondary/Daily.js
+++ src/components/secondary/Daily.js
@@ -10,7 +10,7 @@ const Daily = ({ day, handleSubmmit }) => {
     useEffect(() => {
         const fetchOutfitsForDay = async () => {
             try {
-                const formattedDate = day.toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' }).split('/').reverse().join('-');
+                const formattedDate = day.toISOString().split('T')[0]; 
                 const response = await axios.get(`${apiUrl}day/aed01e26-1d1b-479e-a2aa-c8acc92f03c0/${formattedDate}`);
                 setOutfits(response.data.outfits);
             } catch (error) {
