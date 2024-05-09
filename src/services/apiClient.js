b59687fe712349b1a5eb0761e396763358db9a17
Mateo_ForeroF <87267672+Naetffy@users.noreply.github.com>
2024-05-09 18:46:51 -0500

    Update apiClient.js

--- src/services/apiClient.js
+++ src/services/apiClient.js
@@ -3,10 +3,7 @@ import axios from 'axios';
 const apiClient = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
     withCredentials: true,
-    timeout: 10000,
-    headers: {
-        'Cookie': `authToken=${document.cookie}`
-    }
+    timeout: 10000
 });
 
 export default apiClient;
