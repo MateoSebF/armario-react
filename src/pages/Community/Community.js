30be361a32de89e915d9e18c1690b62ab640af74
Erick <e.santiagom.s@gmail.com>
2024-05-20 20:03:31 -0500

    Tinder Community

--- src/pages/Community/Community.js
+++ src/pages/Community/Community.js
@@ -10,9 +10,11 @@ import './Community.css';
 import apiClient from '../../services/apiClient';
 import Snackbar from '@mui/material/Snackbar';
 import Alert from '@mui/material/Alert';
-
+import TinderCard from 'react-tinder-card';
+import { useMediaQuery } from 'react-responsive';
 
 const Community = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
   const isInitialMount = useRef(true);
   const [mainFocus, setMainFocus] = useState({
     image: '/images/shirt.png',
@@ -40,8 +42,10 @@ const Community = () => {
   const [description, setDescription] = useState(
     'Descripción temporal de la prenda\nName: Example shirt\nColor: White\nSize: M\nType: SHIRT'
   );
-  const [noContent, setNoContent] = useState(false); // Nuevo estado
+  const [noContent, setNoContent] = useState(false);
   const [showRefreshSnackbar, setShowRefreshSnackbar] = useState(false);
+  const [showLike, setShowLike] = useState(false);
+  const [showDislike, setShowDislike] = useState(false);
 
   useEffect(() => {
     if (isInitialMount.current) {
@@ -72,16 +76,37 @@ const Community = () => {
     }
   }, []);
 
+  const onSwipe = (direction) => {
+    console.log('You swiped: ' + direction);
+    if (direction === 'right') {
+      handleLike();
+    } else if (direction === 'left') {
+      handleDislike();
+    }
+      setMainFocusPosition('center');
+  };
+  
+
+  const [mainFocusPosition, setMainFocusPosition] = useState('center');
+
+  const onCardLeftScreen = (myIdentifier, index) => {
+    console.log(myIdentifier + ' left the screen')
+    if (index === 0) {
+      setMainFocusPosition('center');
+    }
+  };
+
   const getTypeFromFocus = (focus) => {
     return focus.type;
   };
 
   const fetchRandomNonLikedClothing = (type) => {
+    console.log('Fetching random non-liked clothing of type:', type);
     apiClient.get(`/clothing/randomNonLiked/byType/${type}`)
       .then(response => {
         if (response.status === 204) {
           console.log('La solicitud se ha procesado con éxito, pero no hay contenido para devolver.');
-          setNoContent(true); // Actualiza el estado a true cuando no hay contenido
+          setNoContent(true);
           setShowRefreshSnackbar(true);
         } else if (typeof response.data === 'object' && response.data !== null) {
           setMainFocus(response.data);
@@ -90,7 +115,7 @@ const Community = () => {
             \nColor: ${response.data.color}
             \nSize: ${response.data.size}
             \nType: ${response.data.type}`);
-          setNoContent(false); // Actualiza el estado a false cuando hay contenido
+          setNoContent(false);
         } else {
           console.error('Invalid response data:', response.data);
         }
@@ -107,8 +132,8 @@ const Community = () => {
       const likeResponse = await apiClient.post(`/clothing/likeClothing`, mainFocus);
 
       if (likeResponse.status === 200) {
-        console.log(description);
         fetchRandomNonLikedClothing(currentType);
+        console.log('Prenda marcada como gustada:', likeResponse.data);
       } else if (likeResponse.status === 204) {
         console.log('No content found after like action');
       } else {
@@ -142,42 +167,80 @@ const Community = () => {
     window.location.reload();
   };
 
-  return (
+ return (
     <div className="col-20">
       <NavBar />
       <div className="container mt-4">
         <div className="row justify-content-center">
           <div className="col-lg-6 col-md-12" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <div style={{ marginBottom: '20px', position: 'relative', maxWidth: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-            {noContent ? (
-                <Snackbar open={showRefreshSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar}>
-                  <Alert severity="warning" onClose={handleCloseSnackbar}>No content to show you. Please change of category.</Alert>
-                </Snackbar>
+              {isMobile ? (
+                  <TinderCard
+                  onSwipe={onSwipe}
+                  onCardLeftScreen={(myIdentifier) => onCardLeftScreen(myIdentifier, 0)}
+                  preventSwipe={['up', 'down']}
+                >
+                  <div style={{ textAlign: mainFocusPosition }}>
+                    {noContent ? (
+                      <Snackbar open={showRefreshSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar}>
+                        <Alert severity="warning" onClose={handleCloseSnackbar}>No content to show you. Please change of category.</Alert>
+                      </Snackbar>
+                    ) : (
+                      <>
+                        <img src={`data:image/jpeg;base64,${mainFocus.image}`} alt="Prenda" style={{ width: '100%', height: 'auto', objectFit: 'contain' }} />
+                        <PrincipalCloth product={mainFocus} />
+                        {showLike && <div style={{ position: 'absolute', top: 20, left: 20, color: 'green' }}>Like!</div>}
+                        {showDislike && <div style={{ position: 'absolute', top: 20, right: 20, color: 'red' }}>Dislike!</div>}
+                      </>
+                    )}
+                  </div>
+                </TinderCard>
               ) : (
                 <>
-                  <img src={`data:image/jpeg;base64,${mainFocus.image}`} alt="Prenda" style={{ width: '100%', height: 'auto', objectFit: 'contain' }} />
-                  <PrincipalCloth product={mainFocus} />
+                  {noContent ? (
+                    <Snackbar open={showRefreshSnackbar} autoHideDuration={6000} onClose={handleCloseSnackbar}>
+                      <Alert severity="warning" onClose={handleCloseSnackbar}>No content to show you. Please change of category.</Alert>
+                    </Snackbar>
+                  ) : (
+                    <>
+                      <img src={`data:image/jpeg;base64,${mainFocus.image}`} alt="Prenda" style={{ width: '100%', height: 'auto', objectFit: 'contain' }} />
+                      <PrincipalCloth product={mainFocus} />
+                    </>
+                  )}
                 </>
               )}
             </div>
           </div>
           <div className="col-lg-6 col-md-12" style={{ height: '800px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', marginTop: '100px' }}>
-            <div style={{ display: 'flex', alignItems: 'center', position: 'relative', height: '50%', marginBottom: '40px' }}>
-              <button onClick={handleLike} style={{ all: 'unset', cursor: 'pointer', width: '100%' }}>
-                <div style={{ position: 'absolute', left: '0', top: '50%', transform: 'translateY(-50%)', zIndex: 2 }}>
-                  <Love product={{ name: '', image: './images/heart.png' }} style={{ color: '#A78262' }} />
+            {isMobile ? (
+              <div style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
+                <button onClick={handleLike} style={{ all: 'unset', cursor: 'pointer', marginRight: '10px' }}>
+                  <Love product={{ name: '', image: './images/heart.png' }} style={{ color: '#A78262', width: '50px', height: '50px' }} />
+                </button>
+                <button onClick={handleDislike} style={{ all: 'unset', cursor: 'pointer' }}>
+                  <Hand product={{ name: '', image: './images/hand.png' }} style={{ color: '#A78262', width: '50px', height: '50px' }} />
+                </button>
+              </div>
+            ) : (
+              <>
+                <div style={{ display: 'flex', alignItems: 'center', position: 'relative', height: '50%', marginBottom: '40px' }}>
+                  <button onClick={handleLike} style={{ all: 'unset', cursor: 'pointer', width: '100%' }}>
+                    <div style={{ position: 'absolute', left: '0', top: '50%', transform: 'translateY(-50%)', zIndex: 2 }}>
+                      <Love product={{ name: '', image: './images/heart.png' }} style={{ color: '#A78262' }} />
+                    </div>
+                    <Like product={mainFocus} style={{ color: '#A78262' }} />
+                  </button>
                 </div>
-                <Like product={mainFocus} style={{ color: '#A78262' }} />
-              </button>
-            </div>
-            <div style={{ display: 'flex', alignItems: 'center', position: 'relative', height: '50%', marginTop: '40px' }}>
-              <button onClick={handleDislike} style={{ all: 'unset', cursor: 'pointer', width: '100%' }}>
-                <div style={{ position: 'absolute', left: '0', top: '50%', transform: 'translateY(-50%)', zIndex: 2 }}>
-                  <Hand product={{ name: '', image: './images/hand.png' }} style={{ color: '#A78262' }} />
+                <div style={{ display: 'flex', alignItems: 'center', position: 'relative', height: '50%', marginTop: '40px' }}>
+                  <button onClick={handleDislike} style={{ all: 'unset', cursor: 'pointer', width: '100%' }}>
+                    <div style={{ position: 'absolute', left: '0', top: '50%', transform: 'translateY(-50%)', zIndex: 2 }}>
+                      <Hand product={{ name: '', image: './images/hand.png' }} style={{ color: '#A78262' }} />
+                    </div>
+                    <Dislike product={mainFocus} style={{ color: '#A78262' }} />
+                  </button>
                 </div>
-                <Dislike product={mainFocus} style={{ color: '#A78262' }} />
-              </button>
-            </div>
+              </>
+            )}
             <div className="secondary-button-container" style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '20px', marginTop: '100px' }}>
               {secondaryFocus.filter(focus => focus).map((focus, index) => (
                 <button key={index} onClick={() => handleFocusSwap(index)} style={{ all: 'unset', cursor: 'pointer' }}>
@@ -193,6 +256,5 @@ const Community = () => {
     </div>
   );
 };
-  
-export default Community;
 
+export default Community;
